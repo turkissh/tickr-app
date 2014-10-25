c7a534cd9fee6b72861daaf81fcb114ce1ec5dc0
Emiliano Di Pierro <emiliano.dipierro@gmail.com>
2014-10-25 05:44:08 -0300

    Modified the callbackurl for heroku

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '494507530694280', // your App ID
 		'clientSecret' 	: '85bed0fc6a7be8d4f7de832fbf9c4e41', // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/api/auth/facebook/callback'
+		'callbackURL' 	: 'http://tickr-app.herokuapp.com/api/auth/facebook/callback'
 	}
 
 };
